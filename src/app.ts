da7132d994b2696cf8595baba79e784efbf3e81d
Richard Essuman <ressuman001@gmail.com>
2024-08-06 21:51:46 +0000

    chore: Add initial project files and structure

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response, NextFunction } from "express";
+
+import todoRoutes from "./routes/todos";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/v1/todos", todoRoutes);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({
+    message: err.message,
+  });
+});
+
+app.listen(3000);
